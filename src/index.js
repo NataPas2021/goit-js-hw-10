526f602e09ebd2ea2a0c2196cc1f73ed174a20c6
NataPas2021 <natapas13@gmail.com>
2022-07-08 14:01:45 +0300

    commit 4

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {alertTooManyMatches, alertNoSuchCountry} from './js/alertsNotifications'
 const DEBOUNCE_DELAY = 300;
 
 const searchBox = document.querySelector('#search-box');
-const listOFCountries = document.querySelector('.country-list')
+const listOFCountries = document.querySelector('.country-list');
 const countryInfoContainer = document.querySelector('.country-info');
 
 searchBox.addEventListener('input', debounce(onInputCountryName, DEBOUNCE_DELAY));
@@ -30,13 +30,20 @@ function onInputCountryName() {
         } 
 
         if (countries.length > 2 && countries.length <= 10) {
-           listOFCountries.insertAdjacentHTML('beforeend', renderCountryList(countries));
+            const listMarkup = countries.map(country => renderCountryList(country));
+            listOFCountries.innerHTML = listMarkup.join('');
+    
+           //listOFCountries.insertAdjacentHTML('beforeend', renderCountryList(country));
         }
 
         if (countries.length === 1) {
-            listOFCountries.innerHTML = '';
+            const markupInfo = countries.map(country => renderCountryInfo(country));
+            console.log(markupInfo)
+            countryInfoContainer.innerHTML = markupInfo;
+            
+            
             //listOFCountries.insertAdjacentHTML('beforeend', renderCountryList(countries));
-            countryInfoContainer.insertAdjacentHTML('beforeend',renderCountryInfo(countries));
+            //countryInfoContainer.insertAdjacentHTML('beforeend',renderCountryInfo(country));
         } 
       })
       .catch(alertNoSuchCountry);
